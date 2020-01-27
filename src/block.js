ccc3663f03a5009d9f9fcb28c877659c544a35c7
Dominik Flisak <dflisak@gmail.com>
2020-01-27 18:01:08 +0100

    Implement basic functionality of Block class
    Added functionality for creating Genesis block, recalculating hash, validation and encoding/decoding data

--- src/block.js
+++ src/block.js
@@ -11,21 +11,28 @@
 
 const SHA256 = require("crypto-js/sha256");
 const hex2ascii = require("hex2ascii");
+const moment = require("moment");
 
 class Block {
+  static createGenesisBlock() {
+    const block = new Block({ type: "GENESIS" }, 0);
+    block.recalculateHash();
+    return block;
+  }
+
   // Constructor - argument data will be the object containing the transaction data
-  constructor(data) {
+  constructor(data, height = 0, previousBlockHash = null) {
     this.hash = null; // Hash of the block
-    this.height = 0; // Block Height (consecutive number of each block)
+    this.height = height; // Block Height (consecutive number of each block)
     this.body = Buffer.from(JSON.stringify(data)).toString("hex"); // Will contain the transactions stored in the block, by default it will encode the data
     this.time = 0; // Timestamp for the Block creation
-    this.previousBlockHash = null; // Reference to the previous Block Hash
+    this.previousBlockHash = previousBlockHash; // Reference to the previous Block Hash
   }
 
   /**
    *  validate() method will validate if the block has been tampered or not.
    *  Been tampered means that someone from outside the application tried to change
-   *  values in the block data as a consecuence the hash of the block should be different.
+   *  values in the block data as a consequence the hash of the block should be different.
    *  Steps:
    *  1. Return a new promise to allow the method be called asynchronous.
    *  2. Save the in auxiliary variable the current hash of the block (`this` represent the block object)
@@ -34,15 +41,9 @@ class Block {
    *  5. Resolve true or false depending if it is valid or not.
    *  Note: to access the class values inside a Promise code you need to create an auxiliary value `let self = this;`
    */
-  validate() {
-    const self = this;
-    return new Promise((resolve, reject) => {
-      // Save in auxiliary variable the current block hash
-      // Recalculate the hash of the Block
-      // Comparing if the hashes changed
-      // Returning the Block is not valid
-      // Returning the Block is valid
-    });
+  async validate() {
+    const validHash = this.calculateHash(this);
+    return this.hash === validHash;
   }
 
   /**
@@ -54,11 +55,25 @@ class Block {
    *  3. Resolve with the data and make sure that you don't need to return the data for the `genesis block`
    *     or Reject with an error.
    */
-  getBData() {
-    // Getting the encoded data saved in the Block
-    // Decoding the data to retrieve the JSON representation of the object
-    // Parse the data to an object to be retrieve.
-    // Resolve with the data if the object isn't the Genesis block
+  async getBData() {
+    return !this.isGenesis ? JSON.parse(hex2ascii(this.body)) : null;
+  }
+
+  get isGenesis() {
+    return this.height === 0;
+  }
+
+  recalculateHash() {
+    this.time = moment().unix();
+    this.hash = this.calculateHash();
+  }
+
+  calculateHash() {
+    const { height, body, time, previousHashBlock } = this;
+
+    return SHA256(
+      JSON.stringify({ height, body, time, previousHashBlock })
+    ).toString();
   }
 }
 
